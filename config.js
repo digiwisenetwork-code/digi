acf55c473c818dad4afc88ca70f39026a6b1505f
kavyaaffiliatenetwork-ship-it <kavya.affiliatenetwork@gmail.com>
2026-02-05 22:31:15 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://hiyxrgbyftirxxbeklrn.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpeXhyZ2J5ZnRpcnh4YmVrbHJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4OTA4MzUsImV4cCI6MjA4NTQ2NjgzNX0.SJj5nCjV1Q914JqdOLF22_wogigntCIhuknwZdGH--k'
+};
