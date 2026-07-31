1b3681058a630eddb116508aee81216f27dea5fc
veerendrakumarmeka <mekaveerendrakumar@gmail.com>
2026-07-31 12:04:43 +0530

    Update Supabase credentials in config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 const CONFIG = {
     APP_NAME: 'DigiWise',
     APP_TAGLINE: 'Performance Marketing Network',
-    SUPABASE_URL: 'https://ifbfqxfdkgptbvhnngfn.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmYmZxeGZka2dwdGJ2aG5uZ2ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMDUxNDksImV4cCI6MjA4NTg4MTE0OX0.B5JKISapbCHNXehGdPs9OxUPKj84YPP6fyTZHHUyyFE'
+    SUPABASE_URL: 'https://cjrynhtvivtqtrwcxeem.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqcnluaHR2aXZ0cXRyd2N4ZWVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NzcxMTEsImV4cCI6MjEwMTA1MzExMX0.oXM7qtYcOorGt9L_rnkQttxGqJsNpsKFwoZOji1lrLU'
 };
